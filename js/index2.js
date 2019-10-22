033280ec1dfeaef5777ae1ae34327aa5a9127656
1729176996 <1729176996@qq.com>
2019-10-22 09:25:01 +0800

    333

--- js/index2.js
+++ js/index2.js
@@ -3,14 +3,19 @@ var vm,loading;
     vm = new Vue({
         el : "#Page",
         data : {
-            
+            app_id:'',
+            auth_code:''
         },
         mounted : function() {
-            
+            var _this = this;
+            if(location.href.indexOf('app_id')){
+                _this.app_id = GetQueryString('app_id');
+                _this.auth_code = GetQueryString('auth_code');
+            }
         },
         methods : {
             test : function() {
-                location.href = 'https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=2019102268526806&scope=auth_user&redirect_uri=https://1729176996.github.io/signs/index.html&state=init';
+                location.href = 'https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=2019102268531811&scope=auth_user&redirect_uri=https://1729176996.github.io/testzfb/index2.html&state=init';
             },
             back:function(){
                 appcan.window.close(-1);
@@ -18,3 +23,9 @@ var vm,loading;
         }
     })
 })($);
+
+function GetQueryString(name){
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);//search,查询？后面的参数，并匹配正则
+     if(r!=null)return  unescape(r[2]); return null;
+}
